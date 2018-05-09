79cb882a0d7e56df5e8a4bade3ad824357ed1c3b
MoncefBkb <31049591+Princeofgame@users.noreply.github.com>
2018-05-09 20:03:37 +0100

    Je suis une poire

---
+++ src/jpu2016/dogfight/view/DogFightView.java
@@ -0,0 +1,14 @@
+package jpu2016.dogfight.view;
+
+public class DogFightView {
+	private int lavie = 42;
+
+	public int getLavie() {
+		return lavie;
+	}
+
+	public void setLavie(int lavie) {
+		this.lavie = lavie;
+	}
+	
+}
